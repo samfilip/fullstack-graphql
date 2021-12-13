4f4dbbdc297c534da8d4e5f8ed7e8ba748853062
samfilip <samjfilip@gmail.com>
2021-12-12 23:05:13 -0800

    mutations

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,11 +5,33 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`
+
 export default function Pets () {
   const [modal, setModal] = useState(false)
+  const { data, loading, error } = useQuery(ALL_PETS)
+  const [{}, ALL_PETS] = useMutation()
 
   const onSubmit = input => {
     setModal(false)
+  }
+
+  if (loading) {
+    return <Loader />
+  }
+
+  if (error) {
+    console.log(error)
+    return <p>error!</p>
   }
   
   if (modal) {
@@ -30,7 +52,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
   )
